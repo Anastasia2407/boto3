2933230d9d0433ff90c15feb6218b556dc40017f
Anastasia-Isakov <stberk20@gmail.com>
2019-10-16 18:44:54 +0300

    boto3

---
+++ boto3/boto-3.py
@@ -0,0 +1,27 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+instances = ec2.create_instances(
+     ImageId='ami-0d5d9d301c853a04a',
+     MinCount=1,
+     MaxCount=2,
+     InstanceType='t2.micro',
+     KeyName='Devops'
+)
+
+# create a new my own ami ec2 instance
+instances = ec2.create_instances(
+     ImageId='ami-055e9447843bc8f47',
+     MinCount=1,
+     MaxCount=1,
+     InstanceType='t2.micro',
+     KeyName='Devops'
+ )
+
+#describing instances
+for instance in ec2.instances.all():
+    print(
+        "Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState {5}\n".format(
+        instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state
+        )
+    )
